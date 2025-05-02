f986c03c0e28c96618c8d43e7eaf47f10d820c27
danielojnorth <danielojnorth@gmail.com>
2025-05-02 14:29:32 +0100

    Adding update to rnaseq_commands.sh

--- rnaseq_commands.sh
+++ rnaseq_commands.sh
@@ -12,10 +12,22 @@ $multiqc -h
 
 ## Code used to run fastqc on cluster
 ### WD:  /project/grte3920/1_linux/2_rnaseq/4_slurm/slurm_fastqc01_05_25.sh
-####   slurm_fastqc01_05_25.sh adapted from slurm_template.sh 
-$ sbatch slurm_fastqc01_05_25.sh
-
+####   slurm_fastqc01_05_25.sh adapted from slurm_template.sh
+sbatch slurm_fastqc01_05_25.sh
 ## Code used to check location in queue
 squeue --me
+
+##Code used to log job to convert .sam file into sorted.bam file
+## note: script assumes specific directory location - uses relative paths 
+sbatch /project/grte3920/1_linux/2_rnaseq/4_slurm/samtools_BAM_SORT_CD4.sh
+
+##Code used to pull reference genome for -a argument of featureCount
+wget ftp://ftp.ensembl.org/pub/release-102/gtf/mus_musculus//Mus_musculus.GRCm38.102.gtf.gz
+
+##Code used to run script to generate index for sorted .bam file, and perform mapping qc on this .bam file
+sbatch /project/grte3920/1_linux/2_rnaseq/4_slurm/CD4_index.sh
+
+##Command used to run featureCount script
+sbatch /project/grte3920/1_linux/2_rnaseq/4_slurm/cd4_featurecount.sh
 
 
